ff3c04f63738d0849f72c495b825114ea630ddf5
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2025-05-13 22:42:14 +0800

    feat: implement auth layout and initial sign up flow with personal info screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Stack } from 'expo-router';
+import { Link, Stack } from 'expo-router';
 import { View } from 'react-native';
 import { Button } from '~/components/ui/button';
 import { Container } from '~/components/ui/container';
@@ -10,22 +10,27 @@ import { Text } from '~/components/ui/text';
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ headerShown: false }} />
+      <Stack.Screen options={{ title: 'Splash Screen', headerShown: false }} />
       <Container>
         <ImageBackground
           source={require('~/assets/illustration.png')}
-          className="flex flex-1"
+          className="flex-1"
           contentPosition="bottom center"
           contentFit="contain">
           <Content>
             <View className="mb-28 flex items-center gap-4">
               <Image source={require('~/assets/logo.png')} className="mb-4 h-56 w-56" />
-              <Text className="font-poppins-bold text-2xl uppercase">
-                Speak Out <Text className="font-poppins-bold text-primary text-2xl">Camanava</Text>
+
+              <Text className="font-poppins-bold text-2xl uppercase text-primary">
+                Speak Out <Text className="font-poppins-bold text-2xl">Camanava</Text>
               </Text>
-              <Button variant="outline">
-                <Text className="font-poppins-regular">Get Started!</Text>
-              </Button>
+
+              <Link href={{ pathname: '/(auth)/sign-up/personal-info' }} asChild>
+                <Button variant="outline">
+                  <Text className="font-poppins-regular">Get Started!</Text>
+                </Button>
+              </Link>
+
               <Text className="font-inter-regular text-muted-foreground">
                 Already have an account?{' '}
                 <Text className="font-inter-bold text-muted-foreground">Sign In</Text>
